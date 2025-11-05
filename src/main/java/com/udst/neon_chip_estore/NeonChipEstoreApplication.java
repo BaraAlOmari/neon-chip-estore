5d98b7202a2162b39bd7b4efa843918c15c8ca22
BaraAlOmari <60300383@udst.edu.qa>
2025-11-05 17:33:39 +0300

    Initial commit

---
+++ src/main/java/com/udst/neon_chip_estore/NeonChipEstoreApplication.java
@@ -0,0 +1,13 @@
+package com.udst.neon_chip_estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonChipEstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeonChipEstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udst/neon_chip_estore/NeonChipEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udst.neon_chip_estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonChipEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
